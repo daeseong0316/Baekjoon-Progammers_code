22d97002c12f05550384913751b26d11bae802d6
daeseong0316 <130436342+daeseong0316@users.noreply.github.com>
2025-09-13 16:57:42 +0900

    [Bronze I] Title: 오르막길, Time: 268 ms, Memory: 19516 KB -BaekjoonHub

---
+++ 백준/Bronze/2846. 오르막길/오르막길.java
@@ -0,0 +1,30 @@
+//Java
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+     
+		int n = sc.nextInt();
+	     
+		int[] arr = new int[n];
+ 	    for(int i=0;i<n;i++) {
+	    	arr[i] = sc.nextInt();
+ 	    }
+	    	
+    	int start = arr[0];	
+    	int max = 0;			
+    	
+    	for(int i=1;i<n;i++) {
+    		if(arr[i] > arr[i-1]) {
+    			max = Math.max(max, arr[i]-start);	
+    		}
+    		else {
+    			start = arr[i];		
+    		}
+    	}
+ 	    
+ 	    System.out.println(max);
+    }
+}
