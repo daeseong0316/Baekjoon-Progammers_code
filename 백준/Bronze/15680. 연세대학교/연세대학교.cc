996c93dfc89d59daa95ba9611724beb4e28a4db5
daeseong0316 <130436342+daeseong0316@users.noreply.github.com>
2025-09-13 17:12:52 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	int a;
+
+	scanf("%d", &a);
+
+	if (a == 0) {
+		printf("YONSEI");
+	}
+	else {
+		printf("Leading the Way to the Future");
+	}
+
+	return 0;
+}
