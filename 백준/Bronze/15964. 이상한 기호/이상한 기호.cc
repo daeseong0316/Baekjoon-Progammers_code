bce39347c9bed3209ee0be15cd5b12069bcc4aaa
daeseong0316 <130436342+daeseong0316@users.noreply.github.com>
2025-09-13 17:13:23 +0900

    [Bronze V] Title: 이상한 기호, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long A, B;
+    long long result = 0;
+    cin >> A >> B;
+
+    result = (A * A) - (B * B);
+
+    cout << result;
+
+    return 0;
+}
