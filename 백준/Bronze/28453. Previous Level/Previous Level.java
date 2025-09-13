c7cd450b919c3e584c744ff7f1237ecb455e0957
daeseong0316 <130436342+daeseong0316@users.noreply.github.com>
2025-09-13 17:16:56 +0900

    [Bronze IV] Title: Previous Level, Time: 184 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/28453. Previous Level/Previous Level.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        
+        for(int i=0;i<n;i++) {
+        	int level = sc.nextInt();
+        	
+        	if(level == 300) {
+        		System.out.print("1 ");
+        	}
+        	else if(level <300 && level >= 275) {
+        		System.out.print("2 ");
+        	}
+        	else if(level <275 && level >= 250) {
+        		System.out.print("3 ");
+        	}
+        	else if(level < 250) {
+        		System.out.print("4 ");
+        	}
+        }
+        
+        sc.close();
+    }
+}
