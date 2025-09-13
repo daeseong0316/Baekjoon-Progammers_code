dc34a97bc0fbc6bcb946a5a9f525a41baa26eba9
daeseong0316 <130436342+daeseong0316@users.noreply.github.com>
2025-09-13 16:50:22 +0900

    [Bronze I] Title: 나머지와 몫이 같은 수, Time: 188 ms, Memory: 17592 KB -BaekjoonHub

---
+++ 백준/Bronze/1834. 나머지와 몫이 같은 수/나머지와 몫이 같은 수.java
@@ -0,0 +1,15 @@
+// Java
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        
+        long value = (long)(n + 1) * n * (n - 1) / 2;
+
+        System.out.println(value);
+    }
+}
