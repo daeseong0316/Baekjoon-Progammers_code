11fd256443d0672ece63c8e7c1837dcb9fe2c71e
daeseong0316 <130436342+daeseong0316@users.noreply.github.com>
2025-09-13 16:53:28 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+	int a, b, c;
+	int money = 0;
+
+	scanf("%d %d %d", &a, &b, &c);
+
+	if (a == b && b == c) {
+		money = 10000 + a * 1000;
+	}
+
+	else if (a == b ) {
+		money = 1000 + a * 100;
+	}
+	else if (b == c ) {
+		money = 1000 + b * 100;
+	}
+	else if (a == c ) {
+		money = 1000 + c * 100;
+	}
+
+	else {
+		if (a > b && a > c)
+			money = a * 100;
+		else if (b > a && b > c)
+			money = b * 100;
+		else if (c > a && c > b)
+			money = c * 100;
+	}
+
+	printf("%d", money);
+
+	return 0;
+}
