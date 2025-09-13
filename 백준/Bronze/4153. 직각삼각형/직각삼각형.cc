ededbf31f44e6beba6ad5e9a2e29a2d8dfc0bdb1
daeseong0316 <130436342+daeseong0316@users.noreply.github.com>
2025-09-13 16:59:30 +0900

    [Bronze III] Title: 직각삼각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int a, b, c;
+
+    while (true) {
+        cin >> a >> b >> c;
+        if (a == 0 && b == 0 && c == 0) {
+            return 0;
+        }
+        else if ((c * c == a * a + b * b) || (b * b == a * a + c * c) || (a * a == c * c + b * b)) {
+            cout << "right" << endl;
+        }
+        else {
+            cout << "wrong" << endl;
+        }
+    }
+
+    return 0;
+}
